90157cd58f02270f7bf520fbba7cdbe043aa6483
Maxence Mahé <maxence.mahe@mines-paristech.fr>
2021-02-08 16:20:01 +0100

    Added jetson code, added docstrings

--- DeepLearning/code_jetson.py
+++ DeepLearning/code_jetson.py
@@ -1,19 +1,22 @@
 import cv2
+import time
 import numpy as np
 import roslibpy
 
 """
-Code implementing Yolov3 and openCV using the camera of the computer (webcam) to detect someone or using the stream
-of the raspberry camera (for that use the cv2.VideoCapture with the link in the parentheses).
+This is the code implemented in the jetson nano
 """
 
-# cap = cv2.VideoCapture('http://172.16.16.171:8080/?action=snapshot')
-cap = cv2.VideoCapture(0)
-classesFile = 'coco.names'
+GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, ' \
+                     'framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, ' \
+                     'format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink ' \
+                     'wait-on-eos=false max-buffers=1 drop=True '
 client = roslibpy.Ros(host='172.16.16.25', port=9090)
 client.run()
-
 talker = roslibpy.Topic(client, '/chatter', 'std_msgs/String')
+cap = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
+
+classesFile = 'coco.names'
 
 whT = 320
 confThreshold = 0.5
@@ -31,12 +34,6 @@ Enreg = False
 
 
 def recvall (clientSocket):
-    """
-        Receives the data from the server, waits for the packet to be full then compares if it is equal to Enreg,
-        which is the green light in case someone has already been identified.
-        :param clientSocket: bytes
-        :return: data: string
-    """
     data = ''
     while True:
         packet = clientSocket.recv(16)
@@ -49,24 +46,17 @@ def recvall (clientSocket):
 
 
 def findObjects (outputs, img):
-    """
-        This functionalities in the output array of YOLO algorithm, analyses the numbers to see whether or not there is a
-        an interesting thing, identifies it, draws the bounding box around it and returns the center of this box.
-        :param outputs: array from the YOLO CNN
-        :param img: array
-        :return: x,y: tuple representing the coordinates of the center of the bounding box
-    """
     hT, wT, cT = img.shape
     bbox = []
     classIds = []
     confs = []
     for output in outputs:
         for det in output:
-            scores = det[5:]  # Only gets the first 5 elements of the array: center (x,y), width, height and probability
+            scores = det[5:]
             classId = np.argmax(scores)
             confidence = scores[classId]
-            if confidence > confThreshold:  # Only consider if confidence is high enough
-                if classNames[classId] == 'person':  # We only want to detect person
+            if confidence > confThreshold:
+                if classNames[classId] == 'person':
                     w, h = int(det[2] * wT), int(det[3] * hT)
                     x, y = int(det[0] * wT - w / 2), int(det[1] * hT - h / 2)
                     bbox.append([x, y, w, h])
@@ -77,25 +67,20 @@ def findObjects (outputs, img):
     if classIds:
         box = bbox[0]
         x, y, w, h = box[0], box[1], box[2], box[3]
-        # mask = np.zeros((hT, wT), np.uint8) --> this part was to create a black mask around the bounding box,
-        # not to be influenced by the background
-        # mask[y:(y + int(1 * h)), x:(x + int(1 * w))] = 255
-        # masked_img = cv2.bitwise_and(img, img, mask=mask)
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
         cv2.putText(img, f'{classNames[classIds[0]].upper()} {int(confs[0] * 100)}%', (x, y - 10),
-                    cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 0, 255), 2)  # Draw the rectangle
+                    cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 0, 255), 2)
+
     else:
-        x, y = -1, -1  # if nothing is detected, return (-1, -1)
+        x, y = -1, -1
     return x, y
 
 
 while True:
-    """
-    While loop, curbed with the time 0.3 sleep if activated. 
-    """
-    # cap = cv2.VideoCapture('http://172.16.16.171:8080/?action=snapshot') --> activate if streaming the camera
-    cap.set(3, 640)
-    cap.set(4, 480)
+    deb = time.time()
+    # cap = cv2.VideoCapture('http://172.16.16.171:8080/?action=snapshot')
+    # cap.set(3, 640)
+    # cap.set(4, 480)
     success, img = cap.read()
     blob = cv2.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], crop=False)
     net.setInput(blob)
@@ -109,9 +94,8 @@ while True:
     data = findObjects(outputs, img)
     data2 = str(data)
     talker.publish(roslibpy.Message({'data': data2}))
-    print('Sending message...')
-    # time.sleep(0.3)
+    print('send')
+    time.sleep(0.3)
 
-talker.unadvertise()
-
+talk.unadvertise()
 client.terminate()
